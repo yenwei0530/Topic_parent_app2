d112892f54828390f6888a3278e9d8f65e2942ff
yunchi1224 <10936028@ntub.edu.tw>
2021-04-09 15:56:33 +0800

    新增[基本資料]介面

--- New_parents/app/src/main/java/com/example/parent_app/user.kt
+++ New_parents/app/src/main/java/com/example/parent_app/user.kt
@@ -1,11 +1,21 @@
 package com.example.parent_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class user : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user)
     }
+
+    fun onLoginClick(view: View) {
+        startActivity(Intent(this,main::class.java))
+    }
+
+
+
+
 }
