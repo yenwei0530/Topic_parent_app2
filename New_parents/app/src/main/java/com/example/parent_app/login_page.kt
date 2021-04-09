202e3ea5e10d44544463157aac5a39c08453bea7
yunchi1224 <10936028@ntub.edu.tw>
2021-04-09 14:43:09 +0800

    修改登入按鈕連結

--- New_parents/app/src/main/java/com/example/parent_app/login_page.kt
+++ New_parents/app/src/main/java/com/example/parent_app/login_page.kt
@@ -2,6 +2,7 @@ package com.example.parent_app
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.login_page.*
@@ -13,4 +14,8 @@ class login_page : AppCompatActivity() {
 
 
         }
+
+    fun login(view: View) {
+        startActivity(Intent(this,main::class.java))
     }
+}
