48936c22bca17a7a980ac61c7baa1e71e5feb471
yunchi1224 <10936028@ntub.edu.tw>
2021-04-09 14:43:58 +0800

    修改主頁介面

--- New_parents/app/src/main/java/com/example/parent_app/main.kt
+++ New_parents/app/src/main/java/com/example/parent_app/main.kt
@@ -1,11 +1,17 @@
 package com.example.parent_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class main : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun user(view: View) {
+        startActivity(Intent(this,user::class.java))
     }
 }
