5e04ae809e1f07cd1511cce5a02aa1d2bfac098b
yunchi1224 <10936028@ntub.edu.tw>
2021-04-11 16:43:07 +0800

    新增孩子的情緒行為介面

--- New_parents/app/src/main/java/com/example/parent_app/activity_child_emotions3.kt
+++ New_parents/app/src/main/java/com/example/parent_app/activity_child_emotions3.kt
@@ -5,17 +5,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 
-class child_emotions : AppCompatActivity() {
+class activity_child_emotions3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_child_emotions)
+        setContentView(R.layout.activity_child_emotions3)
     }
-
     fun onLoginClick(view: View) {
-        startActivity(Intent(this,main::class.java))
+        startActivity(Intent(this,activity_child_emotions2::class.java))
     }
 
     fun next(view: View) {
-        startActivity(Intent(this,activity_child_emotions2::class.java))
+        startActivity(Intent(this,activity_child_emotions4::class.java))
     }
 }
