5d4b4c4bd54c958a5729fdc7a0ce42481ff6d11f
yunchi1224 <10936028@ntub.edu.tw>
2021-04-30 13:25:40 +0800

    新增[孩子最近的表現]介面

--- New_parents/app/src/main/java/com/example/parent_app/child_performance.kt
+++ New_parents/app/src/main/java/com/example/parent_app/child_performance.kt
@@ -2,10 +2,13 @@ package com.example.parent_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class child_performance : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_child_performance)
     }
+
+    fun onLoginClick(view: View) {}
 }
