6443915d0d86167420ba10dc2456134bf3a11445
yunchi1224 <10936028@ntub.edu.tw>
2021-04-30 13:35:44 +0800

    新增[您與孩子的互動]介面

--- New_parents/app/src/main/java/com/example/parent_app/child_interaction.kt
+++ New_parents/app/src/main/java/com/example/parent_app/child_interaction.kt
@@ -2,10 +2,13 @@ package com.example.parent_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class child_interaction : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_child_interaction)
     }
+
+    fun onLoginClick(view: View) {}
 }
