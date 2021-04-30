5fffc82c54039e103cb487a987c5b22649c7066a
yunchi1224 <10936028@ntub.edu.tw>
2021-04-30 16:45:31 +0800

    新增[填寫紀錄]介面

--- New_parents/app/src/main/java/com/example/parent_app/record.kt
+++ New_parents/app/src/main/java/com/example/parent_app/record.kt
@@ -2,10 +2,15 @@ package com.example.parent_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class record : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_record)
     }
+
+    fun child_emotions(view: View) {}
+    fun child_performance(view: View) {}
+    fun child_interaction(view: View) {}
 }
