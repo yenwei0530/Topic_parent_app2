ce73a8a219e9c15ecd76847784c0903b43626294
yunchi1224 <10936028@ntub.edu.tw>
2021-07-15 21:25:41 +0800

    新增回饋介面

---
+++ New_parents/app/src/main/java/com/example/parent_app/give_back.kt
@@ -0,0 +1,11 @@
+package com.example.parent_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class give_back : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_give_back)
+    }
+}
