db313e566390ec79139f5511523e738747a525e2
10936034 <10936034@ntub.edu.tw>
2021-07-18 22:52:53 +0800

    登入/使用紀錄 Sql code  未完成 (已//)

--- New_parents/app/src/main/java/com/example/parent_app/login_page.kt
+++ New_parents/app/src/main/java/com/example/parent_app/login_page.kt
@@ -2,44 +2,75 @@ package com.example.parent_app
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.login_page.*
+import java.util.ArrayList
+import java.util.HashMap
+import java.util.*
 
 class login_page : AppCompatActivity() {
+
+
+    private val DB_NAME = "treatment.db"
+    private val TABLE_NAME = "parent"
+    private val DB_VERSION = 1
+    var mDBHelper: SQLiteDataBaseHelper? = null
+
+    var arrayList = ArrayList<HashMap<String, String>>() //取得所有資料
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_page)
 
 
-        }
-    fun forgot(view: View)
-    {
+        mDBHelper = SQLiteDataBaseHelper(this, DB_NAME, null, DB_VERSION, TABLE_NAME) //初始化資料庫
+        Thread {
+            val con = MysqlCon()
+            con.run()
+            Log.v("OK", "LOGIN con ok")
+        }.start()
 
-    }
-    fun login(view: View) {
-        /*
-        if (username_input.text != null && username_input.length()>0)
-        {
-            if (pass.length() > 0) {
-                startActivity(Intent(this, main::class.java))
+        //建立共用變數類別
+        val gv: GlobalVariable = applicationContext as GlobalVariable
+
+        //宣告Button物件
+        val login = findViewById<TextView>(R.id.login)
+
+        //宣告EditText物件
+        val UserId = findViewById<EditText>(R.id.username_input)
+        val Password = findViewById<EditText>(R.id.pass)
+
+        login.setOnClickListener {
+            /*
+                if (mDBHelper!!.checkparent(
+                        UserId.text.toString(),
+                        Password.text.toString()
+                    ) == "T"
+                ) {
+
+                    //gv.setuser(UserId.text.toString())
+                    //gv.setpassword(Password.text.toString())
+                    //arrayList = mDBHelper!!.parent(UserId.text.toString())
+                    //gv.setname(arrayList[0]["name"])
+                    //gv.setsex(arrayList[0]["sex"])
+
+                    Log.v("LOGIN", "T")
+                    val intent = Intent(this@login_page, main::class.java)
+                    startActivity(intent)
+                } else
+                    Log.e("LOGIN", "F" + "/" + UserId.text + "/" + Password.text)
+                    Toast.makeText(this@login_page, "帳號密碼錯誤!!!", Toast.LENGTH_LONG).show()
+             */
+
+                val intent = Intent(this@login_page, main::class.java)
+                startActivity(intent)
             }
-            else{
-                Toast.makeText(this, "請輸入密碼", Toast.LENGTH_LONG).show()
-            }
-        }
-        else if (username_input.text == null)
-        {
-            Toast.makeText(this, "請輸入帳號", Toast.LENGTH_LONG).show()
-        }
-        else
-        {
-            Toast.makeText(this, "請輸入帳號密碼", Toast.LENGTH_LONG).show()
-        }
 
-        */
-
-        startActivity(Intent(this,main::class.java))
+        }
     }
-}
